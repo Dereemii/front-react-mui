56e8a21d455d380b7f567abcc062c93c5499206d
Leslie Herrera - Dereemii <leshermira@gmail.com>
2024-10-10 20:42:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import Button from '@mui/material/Button'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import Container from '@mui/material/Container'
+
+function App() {
+  return (
+   
+    <Container maxWidth="xl">
+    <Button variant="contained">Hello world</Button>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box> </Container>
+  )
+}
+
+export default App
